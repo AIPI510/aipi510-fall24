77de12e4b6fe129bf105b919f8a8aa923238b481
vihaannnn <vihaan.nama@outlook.com>
2024-08-28 19:11:36 -0400

    Created and began to use testing suite

--- ta1-grok-and-bert/test/test_api_news.py
+++ ta1-grok-and-bert/test/test_api_news.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import pytest
-from workingDirectory.api_news import fetch_articles
+from workingDirectory.api_news import fetch_articles,convert_and_save_dataframe, preprocess_df
 
 @pytest.fixture
 def getArticles():
@@ -9,3 +9,15 @@ def getArticles():
 
 def test_fetch_articles(getArticles):
     assert len(getArticles) > 0, "Failed to fetch"
+
+def test_createDF(getArticles):
+    import os
+    convert_and_save_dataframe(getArticles,"test/articles.csv")
+    assert os.path.exists('test/articles.csv'), "Failed to create and save dataframe"
+
+
+def test_preprocessDF(getArticles):
+    import os
+    df = convert_and_save_dataframe(getArticles,"test/articles.csv")
+    df = preprocess_df(df,"test/preprocessed_articles.csv")
+    assert os.path.exists('test/preprocessed_articles.csv'), "Failed to pre process and save dataframe"
