6a7e6671a40dcbf3d03cccaf29a8b9c84cd9a7d2
Shaunak Badani <shaunak.badani@research.iiit.ac.in>
2024-08-28 23:54:03 -0400

    Implement converting to dataframe and storing it to csv

--- api_trivia.py
+++ api_trivia.py
@@ -1,5 +1,46 @@
 import requests
 import pandas as pd
 
-response = requests.get("https://opentdb.com/api.php?amount=10")
-print(response.json())
+
+def call_api():
+    """
+    Calls the Trivia API
+
+    Returns: Results array
+    """
+    response = requests.get("https://opentdb.com/api.php?amount=10")
+    response_json = response.json()
+    
+    if response_json.get('response_code') == 0:
+        results = response.json().get('results')
+        return results
+    else:
+        raise ConnectionError("Can't establish a connection to the Open Trivia API!")
+    
+
+def make_dataframe(questions):
+    """
+    Make a data-frame object out of the questions array
+
+    Returns: A data-frame object with headers corresponding to the data returned
+    """
+    for idx, question in enumerate(questions):
+        incorrect_answers = question.get('incorrect_answers')
+        for idx, incorrect_answer in enumerate(incorrect_answers):
+            question[f"incorrect_answer_{idx + 1}"] = incorrect_answer
+        question.pop("incorrect_answers")
+    return pd.DataFrame(questions)
+
+
+def save_df_to_csv(dataframe, filename):
+    """
+    Saves the dataframe to csv format
+    """
+    dataframe.to_csv(filename, index = False)
+        
+
+
+if __name__ == "__main__":
+    results = call_api()
+    processed_dataframe = make_dataframe(results)
+    save_df_to_csv(processed_dataframe, "trivia_questions.csv")
