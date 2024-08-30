5a474d784eb00e20d2696f2700198e3bbfcbb2dc
haranku16 <haranku16@users.noreply.github.com>
2024-08-30 14:49:25 -0400

    Cleaning up extra files for pubmed API as instructed

--- api_pubmed.py
+++ api_pubmed.py
@@ -1,5 +1,24 @@
 from pubmed_utils.pubmed import PubMed
 from pprint import pprint
+import requests
+
+'''
+References Used:
+* https://ualibweb.github.io/UALIB_ScholarlyAPI_Cookbook/src/python/pubmed.html
+* https://www.ncbi.nlm.nih.gov/books/NBK25500/
+'''
+class PubMed:
+    # Base URL for querying the PubMed API
+    base_url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils'
+
+    def get_summary(self, id: str):
+        '''
+        Gets the summary for an article of a given id from the PubMed database.
+
+        Parameters:
+            id (string) - Unique identifier of an article in the PubMed database.
+        '''
+        return requests.get(f'{self.base_url}/esummary.fcgi?db=pubmed&id={id}&retmode=json').json()
 
 pubmed = PubMed()
 
