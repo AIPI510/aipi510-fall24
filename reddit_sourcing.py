e93de97b4e6374296d4229574129b84c888256a2
tonywang638156 <luoli.wang@richmond.edu>
2024-08-30 23:31:39 -0400

    build the CLT
    build the CLT to specify the Reddit post URL.

--- reddit_sourcing.py
+++ reddit_sourcing.py
@@ -1,51 +1,57 @@
+import praw
+import pandas as pd
+import argparse
+
 # We use ChatGPT and link below for instruction 
 # https://www.google.com/search?client=firefox-b-1-d&q=how+to+scrape+data+using+reddit+api+in+python#fpstate=ive&vld=cid:615db2be,vid:U9Ogh1OGP-g,st:0
 
 
-"""
-    This is a script of wrapper of Reddit API using PRAW without outputing to CSV File and User CLI
-"""
+# set credentials of Reddit API
+my_client_id = "LGtqEAEdrUfv2W3f0cw_rQ"
+my_client_secret = "fc4HLd_n_5cbNyp3-ZO1yXPEVBI46g"
+my_user_agent = "teamname"
 
-import praw
-import pandas as pd
-
-# set creditials of Reddit API
-my_client_id="LGtqEAEdrUfv2W3f0cw_rQ"
-my_client_secret="fc4HLd_n_5cbNyp3-ZO1yXPEVBI46g"
-my_user_agent="teamname"
-
-# initialize PAW
-
+# Initialize PRAW
 reddit = praw.Reddit(
     client_id=my_client_id,
     client_secret=my_client_secret,
     user_agent=my_user_agent
 )
 
-# set url
-url_towrap="https://www.reddit.com/r/EngineeringStudents/comments/1f53a5b/finally_left_my_company_is_this_normal/"
+# Function to scrape Reddit post
+def scrape_reddit_post(url):
+    # Get the Reddit post submission
+    submission = reddit.submission(url=url)
 
-submission=reddit.submission(url=url_towrap)
+    # Print title, content, and comments of the Reddit post
+    print("Title of the Reddit post:", submission.title)
+    print("Content of the Reddit post:", submission.selftext)
+    
+    print("\nComments of the Reddit post:")
+    for comment in submission.comments:
+        print(comment.body)
 
-# print title ,content and comments of this reddit post
- 
-print("Title of the reddit post:",submission.title)
-print("Content of the reddit post:",submission.selftext)
+    # Put all data into a dictionary
+    data = {
+        'Title': [submission.title if i == 0 else None for i in range(len(submission.comments))],
+        'Body': [submission.selftext if i == 0 else None for i in range(len(submission.comments))],
+        'Comments': [comment.body for comment in submission.comments]
+    }
 
-print("Comment of the reddit post:")
-for comment in submission.comments:
-    print(comment.body)
+    # Put data into a DataFrame
+    df = pd.DataFrame(data)
 
+    # Output the DataFrame as CSV
+    df.to_csv("reddit.csv", index=False)
+    print("\nData saved to 'reddit.csv'.")
 
-# put all data into a dictionary
-data={
-    'Title':[submission.title if i==0 else None for i in range(len(submission.comments)) ],
-    'Body':[submission.selftext if i==0 else None for i in range(len(submission.comments))],
-    'Comments':[comment.body for comment in submission.comments]
-}
+# Set up argument parser
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Reddit post scraper using PRAW.')
+    parser.add_argument('url', type=str, help='URL of the Reddit post to scrape')
+    
+    # Parse the command-line arguments
+    args = parser.parse_args()
 
-# put data into a Dataframe
-df=pd.DataFrame(data)
-
-# output the dataframe as csv
-df.to_csv("reddit.csv",index=False)
+    # Call the function with the given URL
+    scrape_reddit_post(args.url)
