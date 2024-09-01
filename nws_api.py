45a57d82158a78682bfad46b6651604274b3c8aa
Stuart Bladon <stuart.bladon@duke.edu>
2024-09-01 13:12:15 -0400

    created timeseries plot

--- nws_api.py
+++ nws_api.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import pydeck as pdk
 import time
+import requests
 
 # @todo: bring this utility code into this file, the assignment seems to imply we should
 # only be merging nws_api.py
@@ -15,6 +16,7 @@ from weather import Forecast, IPGeo
 # the callback will always run before the rest of your script. For details on the Callbacks
 # API, please refer to our Session State API Reference Guide.
 #
+
 
 def display_chart(df, column): 
     """
@@ -38,6 +40,27 @@ def display_observation_map(df):
     Render the map!
     """
     st.map(df, size=20, color="#0044ff")
+
+def fetch_forecast_data(url):
+    response = requests.get(url)
+    response.raise_for_status()  
+    return response.json()  
+
+def parse_forecast_json(json_data):
+    periods = json_data['properties']['periods']
+    data = []
+    for period in periods:
+        data.append({
+            'time': pd.to_datetime(period['startTime']),
+            'temperature': period['temperature']
+        })
+    
+    df = pd.DataFrame(data)
+    df.set_index('time', inplace=True)
+    return df
+
+def temp_graph(df):
+    st.line_chart(df['temperature'])
 
 @st.cache_data
 def get_location(): 
@@ -91,6 +114,7 @@ if 'button1' not in st.session_state:
     st.session_state.button1 = False
     st.session_state.button2 = False
     st.session_state.button3 = False
+    st.session_state.button4 = False
 
 def click_button1():
     st.session_state.button1 = True
@@ -100,6 +124,9 @@ def click_button2():
 
 def click_button3():
     st.session_state.button3 = True
+
+def click_button4():
+    st.session_state.button4 = True
 
 forecast = None
 st.subheader(":office: Serving NWS Office")
@@ -129,14 +156,25 @@ if st.session_state.button1:
         st.button("Retrieve weather stations", on_click=click_button3)
         if st.session_state.button3: 
             st.subheader('Current observations')
-            observations = get_observations(forecast, stations['station']) 
+            observations = get_observations(forecast, stations['station'])
             
             # @todo, this isn't right... we need to display a map 
             display_chart(observations, 'temp')
 
             # @todo: render some time series data from teh get_forecast method which I think is brokwn at the moment
-            st.subheader("Developer Notes")
+            stream("Click the button to see forecast data for your local station.")
 
-            stream("The NWS API is documented with an OpenAPI UI [here](), however it's use is not straightforward. The most intuitive way\
+            st.button("Get forecast", on_click=click_button4)
+            if st.session_state.button4:
+                forecast_url = forecast.data['forecastHourly']  
+                forecast_json = fetch_forecast_data(forecast_url)
+                tempdata = parse_forecast_json(forecast_json)
+                temp_graph(tempdata)
+                
+                st.subheader("Developer Notes")
+                
+                
+                stream("The NWS API is documented with an OpenAPI UI [here](), however it's use is not straightforward. The most intuitive way\
                     to learn about the API is to visit the [Point Forecast site](https://www.weather.gov/forecastpoints/) and click around \
                     with your browser's developer tools turned on [and monitoring the network traffic](https://developer.chrome.com/docs/devtools/network).")
+        
