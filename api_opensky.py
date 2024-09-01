b3271cb49b82ef61b239c5488d2d0f0640b63f8b
YKH2020 <yash.hegde.2020@gmail.com>
2024-08-31 20:48:15 -0400

    Added 2 functions to find top 10 most crowded coords

--- api_opensky.py
+++ api_opensky.py
@@ -1,14 +1,32 @@
 import sys
 from opensky_api import OpenSkyApi
 
-def main(*argv):
-    """
-    Entry Point
-    """
+def coords_to_dict():
     api = OpenSkyApi()
-    s = api.get_states()
-    print(s)
+    states = api.get_states()
 
+    coords_freqs = {}
+    for state in states.states:
+        if state.latitude is not None and state.longitude is not None:
+            latitude_cell = int(state.latitude)
+            longitude_cell = int(state.longitude)
+
+            cell = (latitude_cell, longitude_cell)
+            if cell in coords_freqs:
+                coords_freqs[cell] += 1
+            else:
+                coords_freqs[cell] = 1
+
+    return coords_freqs
+
+def top10_airspaces(coords_freqs):
+    sorted_by_values = dict(sorted(coords_freqs.items(), key=lambda item: item[1], reverse=True)[:10])
+    for cell, freq in sorted_by_values.items():
+        print(f"Cell: {cell}, Frequency: {freq}")
+
+def main():
+    coords_freqs = coords_to_dict()
+    top10_airspaces(coords_freqs)
+        
 if __name__ == "__main__":
-    main(sys.argv)
-
+    main()
