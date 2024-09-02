8a7fdbeb0c3371e602161305476eb0ca0289eee6
haranku16 <haranku16@users.noreply.github.com>
2024-09-02 17:25:23 -0400

    Removing unused get_summary method from PubMed

--- api_pubmed.py
+++ api_pubmed.py
@@ -23,15 +23,6 @@ class PubMed:
     # Base URL for querying the PubMed API
     base_url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils'
 
-    def get_summary(self, id: str):
-        '''
-        Gets the summary for an article of a given id from the PubMed database.
-
-        Parameters:
-            id (string) - Unique identifier of an article in the PubMed database.
-        '''
-        return requests.get(f'{self.base_url}/esummary.fcgi?db=pubmed&id={id}&retmode=json').json()
-
     def query(self, term):
         return requests.get(f'{self.base_url}/esearch.fcgi?db=pubmed&term={term}&retmode=json').json()
 
