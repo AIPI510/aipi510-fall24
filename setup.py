fbeb0a7fbc1de86eea7c6cbef27f23a1ab61cee2
Jesse Warren <jfw224@lehigh.edu>
2024-09-02 14:14:10 -0400

    merged code together

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='pokeapi_assignment',
     version='0.1',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    py_modules=['main'],
     install_requires=[],
     entry_points='''
         [console_scripts]
-        pokeapi=main:main
+        pokeapi=pokeapi:main
     ''',
 )
