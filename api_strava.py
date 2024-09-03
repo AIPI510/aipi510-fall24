4076b5684e3c1f92b0fbed1c79ba9c5e87798d71
Harshitha Rasamsetty <43064332+harshitha-ras@users.noreply.github.com>
2024-09-03 11:04:14 -0400

    Update api_strava.py

--- api_strava.py
+++ api_strava.py
@@ -8,6 +8,14 @@ if __name__=='__main__':
     We use a desktop app called QGIS to visualise the Strava data. 
     This app lets us download our user data in format of a Shapefile. We use this Shapefile to plot a graph.
     We are reading the Shapefile using geopandas package and plot the data in a graph using matplotlib
+
+
+    Requirements.txt:
+    geopandas==0.12.2
+    matplotlib==3.8.0
+
+    Please install requirements.txt before running this script.
+
     """
     # Load the shapefile
     gdf = gpd.read_file("./shp/recent_activities.shp")
