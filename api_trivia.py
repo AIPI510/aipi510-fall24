8eff1f0214a70209d7f8bcf1df148e067929a180
Shaunak Badani <32260623+shaunak-badani@users.noreply.github.com>
2024-09-08 21:26:29 -0700

    TA #1 - Team Impasta: Using the Open Trivia Database API (Dayeon Kang, Shaunak Badani) (#12)
    * add new file
    
    * implemented request to API
    
    * Implement converting to dataframe and storing it to csv
    
    * Remove csv file and ask gitignore to exclude csv files
    
    * Revert changing of requirements.txt ; The original requirements don't work on macOs, but they work on ubuntu
    
    * Add resources used ; pytests added
    
    * Update README, add tests, clean up implementation
    
    * Fixed tabspaces
    
    * Remove redundant print statement
    
    * Fix default URL
    
    * Add Team name and member names
    
    * Delete test file: Not needed as per rubrics of the assignment
    
    * Update README: add virtual env setup instructions
    
    * Update README
    
    * Delete changes to README and remove gitignore file
    
    * Add running instructions to the api_trivia.py file
    
    ---------
    
    Co-authored-by: Dayeon <159202769+dayeon3117@users.noreply.github.com>
    Co-authored-by: Shaunak Badani <shaunak.badani@duke.edu>

---
+++ api_trivia.py
@@ -0,0 +1,116 @@
+# Resources used:
+# Requests API : https://realpython.com/python-requests/
+# Pandas API : https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_csv.html 
+#    (Along with related web-pages in pandas documentation for other references)
+# JSON API : https://www.geeksforgeeks.org/how-to-remove-key-value-pair-from-a-json-file-in-python/
+"""
+How to run:
+```
+python3 -m venv ../ta1
+source ../ta1/bin/activate
+pip install -r requirements.txt
+```
+
+Running the program:
+```
+usage: python3 api_trivia.py [-h] [--category {9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32}] [-n N]
+Get questions from the Open Trivia API
+options:
+  -h, --help            show this help message and exit
+  --category {9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32}
+                        {"9": "General Knowledge", "10": "Entertainment: Books", "11": "Entertainment: Film", "12": "Entertainment: Music", "13": "Entertainment: Musicals & Theatres",
+                        "14": "Entertainment: Television", "15": "Entertainment: Video Games", "16": "Entertainment: Board Games", "17": "Science & Nature", "18": "Science: Computers",
+                        "19": "Science: Mathematics", "20": "Mythology", "21": "Sports", "22": "Geography", "23": "History", "24": "Politics", "25": "Art", "26": "Celebrities", "27":
+                        "Animals", "28": "Vehicles", "29": "Entertainment: Comics", "30": "Science: Gadgets", "31": "Entertainment: Japanese Anime & Manga", "32": "Entertainment: Cartoon &
+                        Animations"}
+  -n N 
+"""
+
+import requests
+import pandas as pd
+import argparse
+import sys
+import json
+
+CATEGORY_MAP = {
+    9: "General Knowledge",
+    10: "Entertainment: Books",
+    11: "Entertainment: Film",
+    12: "Entertainment: Music",
+    13: "Entertainment: Musicals & Theatres",
+    14:	"Entertainment: Television",
+    15: "Entertainment: Video Games",
+    16: "Entertainment: Board Games",
+    17: "Science & Nature",
+    18: "Science: Computers",
+    19: "Science: Mathematics",
+    20: "Mythology",
+    21:	"Sports",
+    22: "Geography",
+    23: "History",
+    24: "Politics",
+    25: "Art",
+    26: "Celebrities",
+    27: "Animals",
+    28: "Vehicles",
+    29: "Entertainment: Comics",
+    30: "Science: Gadgets",
+    31: "Entertainment: Japanese Anime & Manga",
+    32: "Entertainment: Cartoon & Animations"
+}
+
+def call_api(arguments):
+    """
+    Calls the Trivia API
+
+    Returns: Results array
+    """
+    URL = "https://opentdb.com/api.php?"
+    if arguments.category is not None:
+        URL += f"&category={arguments.category}"
+    URL += f"&amount={arguments.n}"
+    response = requests.get(URL)
+    response_json = response.json()
+    
+    if response_json.get('response_code') == 0:
+        results = response.json().get('results')
+        return results
+    else:
+        raise ConnectionError("Can't establish a connection to the Open Trivia API!")
+    
+
+def make_dataframe(questions):
+    """
+    Make a data-frame object out of the questions array
+
+    Returns: A data-frame object with headers corresponding to the data returned
+    """
+    for idx, question in enumerate(questions):
+        incorrect_answers = question.get('incorrect_answers')
+        for idx, incorrect_answer in enumerate(incorrect_answers):
+            question[f"incorrect_answer_{idx + 1}"] = incorrect_answer
+        question.pop("incorrect_answers")
+    return pd.DataFrame(questions)
+
+
+def save_df_to_csv(dataframe, filename):
+    """
+    Saves the dataframe to csv format
+    """
+    dataframe.to_csv(filename, index = False)
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(description = 'Get questions from the Open Trivia API')
+    parser.add_argument('--category', type = int, choices = list(range(9, 33)),
+                help = json.dumps(CATEGORY_MAP), default = None)
+    parser.add_argument('-n', type = int,
+                help = "Number of questions to fetch", default = 10)
+    arguments = parser.parse_args(args)
+    return arguments
+
+
+if __name__ == "__main__":
+    arguments = parse_args(sys.argv[1:])
+    results = call_api(arguments)
+    processed_dataframe = make_dataframe(results)
+    save_df_to_csv(processed_dataframe, "trivia_questions.csv")
