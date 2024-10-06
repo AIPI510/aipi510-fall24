b390fae856e8379d33cd24e50dff7f0ff41c2297
Matana P <140636857+beungz@users.noreply.github.com>
2024-10-07 05:18:25 +0700

    Minor revisions in unpaired-t-test.py

--- unpaired-t-test.py
+++ unpaired-t-test.py
@@ -7,21 +7,21 @@ import pytest
 
 
 def get_cli_argument():
-    """ArgumentParser to accept inputs from user via command line: sample size, mean & variance for the two randomly generated samples"""
-    parser = argparse.ArgumentParser(description="""Create two samples with random normal distribution based on specified mean, variance, and sample size, 
-                                     then perform unpaired t-test to assess whether there is a significant difference in the mean of the two samples""")
-    parser.add_argument("--n1", type=int, dest="n1", default = "100", help="Sample 1: Sample Size")
-    parser.add_argument("--n2", type=int, dest="n2", default = "100", help="Sample 2: Sample Size")
-    parser.add_argument("--m1", type=float, dest="m1", default = "10", help="Sample 1: Mean")
-    parser.add_argument("--m2", type=float, dest="m2", default = "10", help="Sample 2: Mean")
-    parser.add_argument("--v1", type=float, dest="v1", default = "1", help="Sample 1: Variance")
-    parser.add_argument("--v2", type=float, dest="v2", default = "1", help="Sample 2: Variance")
+    """ArgumentParser to accept inputs from user via command line: sample size, mean & variance for the two randomly generated sample groups"""
+    parser = argparse.ArgumentParser(description="""Create two sample groups with random normal distribution based on specified mean, variance, and sample size, 
+                                     then perform unpaired t-test to assess whether there is a significant difference in the mean of the two sample groups""")
+    parser.add_argument("--n1", type=int, dest="n1", default = "100", help="Sample Group 1: Sample Size")
+    parser.add_argument("--n2", type=int, dest="n2", default = "100", help="Sample Group 2: Sample Size")
+    parser.add_argument("--m1", type=float, dest="m1", default = "10", help="Sample Group 1: Mean")
+    parser.add_argument("--m2", type=float, dest="m2", default = "10", help="Sample Group 2: Mean")
+    parser.add_argument("--v1", type=float, dest="v1", default = "1", help="Sample Group 1: Variance")
+    parser.add_argument("--v2", type=float, dest="v2", default = "1", help="Sample Group 2: Variance")
     args = parser.parse_args()  
     return args
 
 
 def create_random_samples(n,m,v):
-    """Create normally-distributed random sample1, with mean m, variance v, and sample size n"""
+    """Generate a normally-distributed random sample group, with mean m, variance v, and sample size n"""
     sample = np.random.normal(loc=m, scale=v, size=n)
 
     # Print sample size, and resulting mean/variance
@@ -32,8 +32,10 @@ def create_random_samples(n,m,v):
 
 
 def normality_test(sample, shapiro_alpha):
-    """Perform Shapiro-Wilk Test of Normality"""
+    """Perform Shapiro-Wilk Test of Normality based on given sample group, and alpha"""
     shapiro_test_stat, shapiro_p_value = scipy.stats.shapiro(sample)
+    
+    # Interpretation
     if shapiro_p_value > shapiro_alpha:
         print("Fail to reject Null Hypothesis: Sample is from the normal distributions")
         sample_is_normal = True
@@ -44,8 +46,10 @@ def normality_test(sample, shapiro_alpha):
 
 
 def levene_test(sample1, sample2, levene_alpha):
-    """Perform Levene's test for homogeneity of variances"""
+    """Perform Levene's test for homogeneity of variances based on two given sample groups, and alpha"""
     levene_w_stats, levene_p_value = scipy.stats.levene(sample1, sample2, center='mean')
+    
+    # Interpretation
     if levene_p_value > levene_alpha:
         print("Fail to reject Null Hypothesis: Homogeneity of Variances")
         variance_is_similar = True
@@ -56,7 +60,7 @@ def levene_test(sample1, sample2, levene_alpha):
 
 
 def unpaired_t_test(sample1, sample2, df, p_value_critical):
-    """Perform Unpaired t-test"""
+    """Perform Unpaired t-test based on two given sample groups, degrees of freedom, and critical p-value"""
     # Calculate t-statistic using scipy
     t_statistic, p_value = scipy.stats.ttest_ind(a=sample1, b=sample2, equal_var=True)
     print("\nCalculation of t-value:")
@@ -72,10 +76,10 @@ def unpaired_t_test(sample1, sample2, df, p_value_critical):
     # Interpretation
     print("\nInterpretation:")
     if t_statistic > t_critical:
-        print("Reject H0: There is significant difference in the mean of the two samples")
+        print("Reject H0: There is significant difference in the mean of the two sample groups")
         reject_h0 = True
     else:
-        print("Fail to reject H0: There is NO significant difference in the mean of the two samples")
+        print("Fail to reject H0: There is NO significant difference in the mean of the two sample groups")
         reject_h0 = False
 
     return reject_h0
@@ -83,27 +87,27 @@ def unpaired_t_test(sample1, sample2, df, p_value_critical):
 
 # Test Functions
 def test_create_random_samples():
-    """Test creating random normal samples, with sample size of 100, mean of 10.0, and variance of 1.0, then check if resulting random sample actually has 100 samples."""
+    """Test creating a random normal sample group, with sample size of 100, mean of 10.0, and variance of 1.0, then check if resulting random sample group actually has 100 samples."""
     assert len(create_random_samples(100,10.0,1.0)) == 100
 
 def test_normality_test():
-    """Test creating random normal samples, with sample size of 1000, mean of 10.0, and variance of 1.0, then perform Normality Test. 
-    As the sample size is large, we can expect that the resulting samples should approximately normally distributed"""
+    """Test generating a random normal sample group, with sample size of 1000, mean of 10.0, and variance of 1.0, then perform Normality Test. 
+    As the sample size is not small, we can expect that the resulting sample group should be approximately normally distributed"""
     assert normality_test(create_random_samples(1000,10.0,1.0), 0.05) == True
 
 def test_levene_test():
-    """Test creating two random normal samples, both with sample size of 1000, mean of 10.0, and variance of 1.0, then perform Levene's Test.
-    As the sample size of the two samples are large, we can expect that the variance of the two random samples should be similar"""
+    """Test generating two random normal samples, both with sample size of 1000, mean of 10.0, and variance of 1.0, then perform Levene's Test.
+    As the sample size of the two sample groups are not small, we can expect that the variance of the two random sample groups should be similar"""
     assert levene_test(create_random_samples(1000,10.0,1.0), create_random_samples(1000,10.0,1.0), 0.05) == True
 
 def test_unpaired_t_test():
-    """Test creating two random normal samples, both with sample size of 1000, mean of 10.0, and variance of 1.0, then perform Unpaired t-test.
-    As the sample size of the two samples are large, we can expect that there is NO significant difference in the mean of the two samples (Fail to reject H0)"""
+    """Test generating two random normal sample groups, both with sample size of 1000, mean of 10.0, and variance of 1.0, then perform Unpaired t-test.
+    As the sample size of the two sample groups are not small, we can expect that there is NO significant difference in the mean of the two sample groups (Fail to reject H0)"""
     assert unpaired_t_test(create_random_samples(1000,10.0,1.0), create_random_samples(1000,10.0,1.0), 10000-2, 0.05) == False
 
 
 def main():
-    """Get sample size, mean & variance, create two random samples, then perform unpaired t-test"""
+    """Get sample size, mean & variance, create two random sample groups, then perform unpaired t-test"""
     # Get all user's arguments
     args = get_cli_argument()
     n1 = args.n1
@@ -113,37 +117,37 @@ def main():
     v1 = args.v1
     v2 = args.v2
     
-    # Create normally-distributed random sample1/sample2 based on given sample size, mean, and variance
-    print("\nSample 1") 
+    # Create normally-distributed random sample1/sample2 groups based on given sample size, mean, and variance
+    print("\nSample Group 1") 
     sample1 = create_random_samples(n1,m1,v1)
-    print("\nSample 2") 
+    print("\nSample Group 2") 
     sample2 = create_random_samples(n2,m2,v2)
 
     # Calculate degrees of freedom  
     df = min(n1,n2) - 2 
 
-    # Plot histograms of the two samples to see if they are Gaussian-like
+    # Plot histograms of the two sample groups to see if they are Gaussian-like
     fig, axes = plt.subplots(1, 3, figsize=(15, 5))
 
     sns.histplot(data=sample1, bins = 20, ax=axes[0], color='blue') 
-    axes[0].set_title('Sample 1 Histogram')
+    axes[0].set_title('Sample Group 1 - Histogram')
 
     sns.histplot(data=sample2, bins = 20, ax=axes[1], color='orange') 
-    axes[1].set_title('Sample 2 Histogram')
+    axes[1].set_title('Sample Group 2 - Histogram')
 
     sns.histplot(data=sample1, bins = 20, ax=axes[2], color='blue') 
     sns.histplot(data=sample2, bins = 20, ax=axes[2], color='orange') 
-    axes[2].set_title('Sample 1 (blue) and Sample 2 (orange)')
+    axes[2].set_title('Sample Group 1 (blue) and Sample Group 2 (orange)')
 
-    fig.suptitle('Histograms of the two samples')
+    fig.suptitle('Histograms of the two Sample Groups')
     plt.show()
 
     # Normality Test: Check Assumption of Normality using Shapiro-Wilk Test of Normality
     print("\nCheck Assumption of Normality:")
     shapiro_alpha = 0.05
-    print("Sample 1") 
+    print("Sample Group 1") 
     sample_is_normal1 = normality_test(sample1, shapiro_alpha)
-    print("Sample 2")
+    print("Sample Group 2")
     sample_is_normal2 = normality_test(sample2, shapiro_alpha)
 
     # Levene's Test: Check Assumption of Homogeneity of Variances
@@ -151,12 +155,12 @@ def main():
     levene_alpha = 0.05
     variance_is_similar = levene_test(sample1, sample2, levene_alpha)
 
-    # Perform unpaired t-test if both samples are approximately normally distributed, and variance are similar
+    # Perform unpaired t-test if both sample groups are approximately normally distributed, and variance are similar
     print("\nUnpaired t-test")
     print("H0 (Null Hypotheses): There is NO significant difference in the mean of the two sample groups")
     print("H1 (Alternative Hypotheses): There is significant difference in the mean of the two sample groups")
     if sample_is_normal1 and sample_is_normal2 and variance_is_similar:
-        print("\nBoth samples are approximately normally distributed, and variance are similar.")
+        print("\nBoth sample groups are approximately normally distributed, and variance are similar.")
         print("Unpaired t-test will then be performed")
         p_value_critical = 0.05
         reject_h0 = unpaired_t_test(sample1, sample2, df, p_value_critical)
@@ -173,9 +177,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-
-
-
-
-
-
