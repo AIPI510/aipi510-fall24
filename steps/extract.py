82999eee476774874af2f2a07cd27873001449c0
sganes21 <sganes21@users.noreply.github.com>
2024-11-15 22:11:21 -0500

    Updated code extract lambda function for Market Data

--- steps/extract.py
+++ steps/extract.py
@@ -1,12 +1,33 @@
+import os
 import json
+import requests
+
+
+
+
+
+
 
 def lambda_handler(event, context):
+
+    # Retrieve the environment variable
+    my_env_var = os.environ.get('MARKETSTACK_API_KEY', 'default_value')
+
+     # Use the environment variable in your code
+    message = f"The value of MY_ENV_VAR is: {my_env_var}"
+
+    url = F"https://api.marketstack.com/v1/eod?access_key={my_env_var}"
+
+    querystring = {"symbols":"AAPL,MSFT,AMZN,TSLA,NVDA"}
+
+    response = requests.get(url, params=querystring)
+
+    print(response.json())
+
+
+    
     return {
         'statusCode': 200,
-        'body': json.dumps({
-            'data': [
-                { 'name': 'Haran Nallasivan' },
-                { 'name': 'Santosh Ganesan' }
-            ]
-        })
+        'body': json.dumps(response.json())
     }
+
