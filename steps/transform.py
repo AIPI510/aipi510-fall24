4ee116b4b47d1ee8a19da36b2b3648a42278d779
sganes21 <sganes21@users.noreply.github.com>
2024-11-17 07:32:04 -0500

    Added initial code to transform data, have not tested

--- steps/transform.py
+++ steps/transform.py
@@ -1,13 +1,15 @@
-import json
+# lambda function assumes that the 'data' key in the API response always contains a list of dictionaries, each representing a stock entry.
 
-def backward(str):
-    return str[::-1]
+import json
+import pandas as pd
+
+
 
 def lambda_handler(event, context):
-    payload = json.loads(event['body'])
+    transform_response = lambda response: pd.DataFrame(json.loads(response)['data'])
+    df = transform_response()
+
     return {
         'statusCode': 200,
-        'body': json.dumps({
-            'data': [backward(data['name']) for data in payload['data']]
-        })
+        'body': json.dumps('Lambda function executed successfully')
     }
