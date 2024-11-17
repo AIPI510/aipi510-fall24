83c7fd5e321e2a519993894cbeffcb7b8f24358a
sganes21 <sganes21@users.noreply.github.com>
2024-11-16 22:50:45 -0500

    Removed whitespaces, renamed variable, and cleaned up code

--- steps/extract.py
+++ steps/extract.py
@@ -2,29 +2,17 @@ import os
 import json
 import requests
 
-
-
-
-
-
-
 def lambda_handler(event, context):
 
     # Retrieve the environment variable
-    my_env_var = os.environ.get('MARKETSTACK_API_KEY', 'default_value')
+    api_key = os.environ.get('MARKETSTACK_API_KEY', 'default_value')
 
      # Use the environment variable in your code
-    message = f"The value of MY_ENV_VAR is: {my_env_var}"
-
-    url = F"https://api.marketstack.com/v1/eod?access_key={my_env_var}"
+    url = F"https://api.marketstack.com/v1/eod?access_key={api_key}"
 
     querystring = {"symbols":"AAPL,MSFT,AMZN,TSLA,NVDA"}
 
     response = requests.get(url, params=querystring)
-
-    print(response.json())
-
-
     
     return {
         'statusCode': 200,
