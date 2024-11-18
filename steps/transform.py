d0ca65c2f89e9b1247c06d32beac7ce26f5f8b5c
sganes21 <sganes21@users.noreply.github.com>
2024-11-17 21:59:08 -0500

    rework lambda function to pass df.to_csv() as body

--- steps/transform.py
+++ steps/transform.py
@@ -2,14 +2,25 @@
 
 import json
 import pandas as pd
+import io
+
 
 
 
 def lambda_handler(event, context):
     transform_response = lambda response: pd.DataFrame(json.loads(response)['data'])
-    df = transform_response()
+    df = transform_response(event)
+
+    # Convert DataFrame to CSV string
+    csv_buffer = io.StringIO()
+    df.to_csv(csv_buffer, index=False)
+    csv_string = csv_buffer.getvalue()
 
     return {
         'statusCode': 200,
-        'body': json.dumps('Lambda function executed successfully')
+        'body': csv_string('Lambda function executed successfully'),
+        'headers': {
+            'Content-Type': 'text/csv',
+            'Content-Disposition': 'attachment; filename=data.csv'
+        }
     }
